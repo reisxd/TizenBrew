7da7859e6b15ac73c80192d60666113b05600a8f
reisxd <29177546+reisxd@users.noreply.github.com>
2024-03-05 00:35:12 +0300

    feat: new ui
    thank you cookie
    
    Co-Authored-By: Cookie <157894049+6-5-5@users.noreply.github.com>

--- tizenbrew-app/TizenBrew/js/index.js
+++ tizenbrew-app/TizenBrew/js/index.js
@@ -4,26 +4,5 @@ window.begin = function() {
     localStorage.setItem("ip", "127.0.0.1");
     if (localStorage.getItem('modules') == null) localStorage.setItem('modules', '[]');
     if (localStorage.getItem('failedStartupAttempts') == null) localStorage.setItem('failedStartupAttempts', '0');
-    if (localStorage.getItem("ip") == null) {
-        // Set IP by prompt
-        selectedItem.innerHTML = `
-        <div class="has-mb-12">
-            <h5 class="has-text-light">Set Server IP</h5>
-            <label class="label">Server IP:</label>
-            <input type="text" class="input" placeholder="192.168.1.3" id="ip">
-        </div>
-        `;
-        // Focus on input
-        const input = document.getElementById("ip");
-        input.focus();
-        input.onblur = () => {
-            // Set IP
-            localStorage.setItem("ip", input.value);
-            alert("Set IP to " + input.value);
-            // Reload page
-            window.location.reload();
-        }
-    } else {
-        connect();
-    }
+    connect();
 }
--- tizenbrew-app/TizenBrew/js/wsClient.js
+++ tizenbrew-app/TizenBrew/js/wsClient.js
@@ -10,7 +10,7 @@ function connect() {
         if (ip.includes(':')) {
             client = new WebSocket(`ws://${ip}`);
         } else {
-            client = new WebSocket(`ws://${ip}:8081`);
+            client = new WebSocket(`ws://127.0.0.1:8081`);
         }
         client.onmessage = onMessage;
         client.onopen = onOpen;
@@ -34,19 +34,18 @@ function onMessage(msg) {
     switch (message.type) {
         case 'debugStatus': {
             if (message.inDebug.tizenDebug) {
+                hideError();
                 localStorage.setItem('failedStartupAttempts', '0');
                 canLaunchModules = message.inDebug.webDebug;
                 send({ type: 'loadModules', modules: JSON.parse(localStorage.getItem('modules')) });
             } else {
                 const failedStartupAttempts = parseInt(localStorage.getItem('failedStartupAttempts'));
-                if (failedStartupAttempts < 3) {
+                if (failedStartupAttempts < 2) {
                     localStorage.setItem('failedStartupAttempts', failedStartupAttempts + 1);
                     send({ type: 'relaunchInDebug', isTizen3, tvIp: webapis.network.getIp() });
                     tizen.application.getCurrentApplication().exit();
                 } else {
-                    document.getElementById('errorDiv').innerHTML = `<div class="alert has-mb-none item" style="background-color: #db2b2b;">
-                    <h1 class="has-text-light">Error: Could not connect to the server after 3 attempts. Are you sure you changed the Host PC IP to 127.0.0.1?</h1>
-                    </div>`;
+                    showError(`Error: Could not connect to the server after 3 attempts. Are you sure you changed the Host PC IP to 127.0.0.1?`);
                     localStorage.setItem('failedStartupAttempts', '0');
                 }
             }
@@ -58,10 +57,10 @@ function onMessage(msg) {
                 document.getElementById('appList').innerHTML = '';
                 for (const module of message.modules) {
                     document.getElementById('appList').innerHTML += `
-                    <div data-packagename="${module.name}" data-appPath="${module.appPath}" class="card has-w-96 is-shadowed column is-one-quarter is-full-mobile is-flex ${firstOne ? 'has-bg-primary' : 'has-bg-secondary'} item">
+                    <div data-packagename="${module.name}" data-appPath="${module.appPath}" class="card ${firstOne ? 'selected' : ''}" tabimdex="0">
                         <div>
-                            <h1 class="has-mb-1 has-text-white">${module.appName}</h1>
-                            <h3 class="is-semitransparent">
+                            <h1>${module.appName}</h1>
+                            <h3>
                                 ${module.description}
                             </h3>
                         </div>
@@ -69,7 +68,7 @@ function onMessage(msg) {
                     `;
                     firstOne = false;
                 }
-                window.selectedItem = document.querySelector(".has-bg-primary");
+                window.selectedItem = document.querySelector(".selected");
                 window.currentRow = selectedItem.parentElement.parentElement;
             } else {
                 document.getElementById('navigateText').innerHTML = "Seems like you haven't installed any modules yet. Use the [GREEN] button to access the module manager.";
@@ -78,11 +77,9 @@ function onMessage(msg) {
             break;
         }
         case 'error': {
-            document.getElementById('errorDiv').innerHTML = `<div class="alert has-mb-none item" style="background-color: #db2b2b;">
-            <h1 class="has-text-light">Error: ${message.message}</h1>
-            </div>`
+            showError(`Error: ${message.message}`)
             setTimeout(() => {
-                document.getElementById('errorDiv').innerHTML = '';
+                hideError();
             }, 5000);
             break;
         }
